99fa6ec8e8bb4223517ba89d882eafd14d805a55
TimOudeAvenhuis <113439801+TimOudeAvenhuis@users.noreply.github.com>
2024-06-07 08:50:01 +0200

    Remake Post and Roles table

--- database/migrations/2024_06_06_130010_create_posts_table.php
+++ database/migrations/2024_06_06_130010_create_posts_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('content');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('posts');
     }
 };
--- database/migrations/2024_06_06_133306_create_roles_table.php
+++ database/migrations/2024_06_06_133306_create_roles_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('role_name');
+            $table->timestamps();
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('roles');
     }
 };
